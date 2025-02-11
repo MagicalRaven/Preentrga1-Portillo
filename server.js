ccd2220341e8e5ae4b2e57952c3c7ff561601b76
Lynne <143041306+MagicalRaven@users.noreply.github.com>
2025-02-11 16:34:02 -0600

    Correcciones

--- server.js
+++ server.js
@@ -2,28 +2,27 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import passport from "./config/passport.js"; // Importar passport después de inicializar app
+import passport from "./config/passport.js"; 
 import authRoutes from "./routes/auth.js";
 
 dotenv.config();
 
-const app = express(); // Aquí se inicializa `app`
+const app = express(); 
 const PORT = process.env.PORT || 3000;
 
-// Middleware
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// Inicializar Passport
+
 app.use(passport.initialize());
 
-// Rutas
+
 app.use("/api/sessions", authRoutes);
 
-// Conectar a MongoDB
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB conectado");
     app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
